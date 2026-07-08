1f39158c2167256939dc8e76dff71bb07d632c2b
nico-log <niko.gl2001@gmail.com>
2026-07-08 15:29:21 +0200

    Shared memory implementation

--- 2D_Conv_Parallel/kernel.cu
+++ 2D_Conv_Parallel/kernel.cu
@@ -12,7 +12,10 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
+#define BLOCK_SIZE 16
 #define FILTER_DIM 3
+#define FILTER_RADIUS 1
+#define TILE_SIZE (BLOCK_SIZE + 2 * FILTER_RADIUS)
 
 void convolution(
 	const unsigned char* input_image,
@@ -120,7 +123,92 @@ __global__ void convolution_kernel(
 	}
 }
 
+__global__ void convolution_kernel_shared(
+	unsigned char* input_image,
+	unsigned char* output_image,
+	int width, int height, int channels,
+	const float* filter_matrix, int filter_dim)
+{
+	// SHARED MEMORY
+	__shared__ float shared_tile[TILE_SIZE][TILE_SIZE][3]; // [y][x][channel]
+
+	// Thread index within the block
+	int t_id = threadIdx.y * blockDim.x + threadIdx.x;
+	int num_threads = blockDim.x * blockDim.y;	// 256 workers per block
+	int num_pixels = TILE_SIZE * TILE_SIZE;		// 324 pixels per tile
+
+	// starting pixel index for this thread in the shared tile
+	int origin_x = blockIdx.x *	BLOCK_SIZE - FILTER_RADIUS;
+	int origin_y = blockIdx.y * BLOCK_SIZE - FILTER_RADIUS;
+
+	// Loading pixels into shared memory
+	for (int i = t_id; i < num_pixels; i += num_threads) {
+
+		// from linear index to 2D coordinates in the shared tile
+		int tile_y = i / TILE_SIZE; // row in the shared tile
+		int tile_x = i % TILE_SIZE;	// col in the shared tile
+
+		// corresponding pixel coordinates in the input image
+		int image_x = origin_x + tile_x;
+		int image_y = origin_y + tile_y;
+
+		for (int c = 0; c < channels; ++c) {
+
+			shared_tile[tile_y][tile_x][c] = 0.0f; // default value for out-of-bounds pixels
+
+			if (image_x >= 0 && image_x < width && image_y >= 0 && image_y < height) {
+				int image_index = (image_y * width + image_x) * channels + c;
+				shared_tile[tile_y][tile_x][c] = input_image[image_index];	
+			}
+		}
+
+	}	
+
+	__syncthreads();
+
+	// CONVOLUTION COMPUTATION
+	// Pixel coordinates for the current thread
+	int x = blockIdx.x * blockDim.x + threadIdx.x;
+	int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+	// filter 3*3 => offset from -1 to +1
+	int offset = filter_dim / 2;
+
+	// guard against out-of-bounds threads
+	if (x < width && y < height) {
+		for (int c = 0; c < channels; ++c) {
+			float sum = 0.0f;
+
+			//filter (fy rows, fx cols)
+			for (int fy = -offset; fy <= offset; ++fy) {
+				for (int fx = -offset; fx <= offset; ++fx) {
+
+					float pixel_value = shared_tile[threadIdx.y + fy + FILTER_RADIUS][threadIdx.x + fx + FILTER_RADIUS][c];					
+
+					int filter_index = (fy + offset) * filter_dim + (fx + offset);
+					float filter_value = filter_matrix[filter_index];
+
+					sum += pixel_value * filter_value;
+
+				}
+			}
+
+			// Clamp the result to [0, 255] and assign it to the output image
+			if (sum < 0.0f) sum = 0.0f;
+			if (sum > 255.0f) sum = 255.0f;
+
+			// Assign the computed value to the output image array (flattened 3D array)
+			int output_index = (y * width + x) * channels + c;
+			output_image[output_index] = static_cast<unsigned char>(sum); //cast float to unsigned char
+		}
+
+	}
+	
+}
+
 int main() {
+
+	std::cout << "2D CONVOLUTION PERFORMANCE COMPARISON\n" << std::endl;
 
 	//FILTERS
 	float blur_filter[FILTER_DIM * FILTER_DIM] = {
@@ -142,7 +230,6 @@ int main() {
 	};
 
 	float* filter_matrix = edge_filter; // Choose the filter to apply
-	int filter_dim = FILTER_DIM; // 3x3 filter
 
 	// =====================================================================
 	// SERIAL CPU IMPLEMENTATION OF 2D CONVOLUTION
@@ -155,8 +242,6 @@ int main() {
 	int width, height, input_channels;
 	int output_channels = 3;
 
-	std::cout << "Loading image: " << input_image_path << std::endl;
-
 	unsigned char* input_image = stbi_load(input_image_path, &width, &height, &input_channels, output_channels);
 
 	if (input_image == nullptr) {
@@ -164,37 +249,31 @@ int main() {
 		return 1;
 	}
 
-	std::cout << "Image loaded: " << width << "x" << height << ", Channels: " << input_channels << "\n" << std::endl;
+	std::cout << input_image_path << " Loaded: " << width << "x" << height << ", Channels: " << input_channels << std::endl;
 
 	// CONVOLUTION
 	// Allocate memory for the output image
 	unsigned char* output_image = new unsigned char[width * height * output_channels];
 
-	std::cout << "Applying convolution filter (CPU)..." << std::endl;
-
 	// Measure the time taken for convolution
 	auto start_time_cpu = std::chrono::high_resolution_clock::now();
 
-	convolution(input_image, output_image, width, height, output_channels, filter_matrix, filter_dim);
+	convolution(input_image, output_image, width, height, output_channels, filter_matrix, FILTER_DIM);
 
 	auto end_time_cpu = std::chrono::high_resolution_clock::now();
 
 	std::chrono::duration<double, std::milli> elapsed_time_cpu = end_time_cpu - start_time_cpu;
-	std::cout << "Serial Convolution (CPU) completed in " << elapsed_time_cpu.count() << " ms" << std::endl;
+	std::cout << "\n1. Serial Convolution (CPU) completed in " << elapsed_time_cpu.count() << " ms" << std::endl;
 
 	// Save the output image
 	int quality = 100;
 	stbi_write_jpg(output_image_path_serial, width, height, output_channels, output_image, quality);
-
-	std::cout << "Output image saved: " << output_image_path_serial << "\n" << std::endl;
 
 	// =====================================================================
 	// PARALLEL GPU IMPLEMENTATION OF 2D CONVOLUTION (CUDA)
 	// =====================================================================
 
 	const char* output_image_path_parallel = "output_image_parallel.jpg";
-
-	std::cout << "GPU memory allocation and transfering..." << std::endl;
 
 	// GPU memory pointers
 	unsigned char* d_input_image = nullptr;
@@ -203,7 +282,7 @@ int main() {
 
 	// Allocate GPU memory for input image, output image, and filter
 	size_t image_size = width * height * output_channels * sizeof(unsigned char);
-	size_t filter_size = 9 * sizeof(float); // 3x3 filter
+	size_t filter_size = FILTER_DIM * FILTER_DIM * sizeof(float); // 3x3 filter
 
 	cudaMalloc((void**)&d_input_image, image_size);
 	cudaMalloc((void**)&d_output_image, image_size);
@@ -213,28 +292,63 @@ int main() {
 	cudaMemcpy(d_input_image, input_image, image_size, cudaMemcpyHostToDevice);
 	cudaMemcpy(d_filter, filter_matrix, filter_size, cudaMemcpyHostToDevice);
 
-	dim3 threadsPerBlock(16, 16); // 16 x 16 = 256 threads per block
+	dim3 threadsPerBlock(BLOCK_SIZE, BLOCK_SIZE); // 16 x 16 = 256 threads per block
 	dim3 numBlocks((width + threadsPerBlock.x - 1) / threadsPerBlock.x, (height + threadsPerBlock.y - 1) / threadsPerBlock.y); // Round up to the nearest block
 
-	auto start_time_gpu = std::chrono::high_resolution_clock::now();
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
 
-	convolution_kernel <<<numBlocks, threadsPerBlock >>> (d_input_image, d_output_image, width, height, output_channels, d_filter, filter_dim);
-	cudaDeviceSynchronize();
+	cudaEventRecord(start);
 
-	auto end_time_gpu  = std::chrono::high_resolution_clock::now();
+	convolution_kernel <<<numBlocks, threadsPerBlock >>> (d_input_image, d_output_image, width, height, output_channels, d_filter, FILTER_DIM);
+	
+	cudaEventRecord(stop);
+	cudaEventSynchronize(stop);
 
-	std::chrono::duration<double, std::milli> elapsed_time_gpu = end_time_gpu - start_time_gpu;
-	std::cout << "Parallel Convolution (GPU) completed in " << elapsed_time_gpu.count() << " ms" << std::endl;
+	float elapsed_time_gpu = 0.0f;
+	cudaEventElapsedTime(&elapsed_time_gpu, start, stop);
+	std::cout << "2. Parallel Convolution (GPU) completed in " << elapsed_time_gpu << " ms" << std::endl;
 
 	cudaMemcpy(output_image, d_output_image, image_size, cudaMemcpyDeviceToHost);
 
 	// Save the output image
 	stbi_write_jpg(output_image_path_parallel, width, height, output_channels, output_image, quality);
 
-	std::cout << "Output image saved: " << output_image_path_parallel << "\n" << std::endl;
+	// =====================================================================
+	// PARALLEL GPU IMPLEMENTATION WITH SHARED MEMORY
+	// =====================================================================
 
-	float speedup = elapsed_time_cpu.count() / elapsed_time_gpu.count();
-	std::cout << "Speedup (CPU time / GPU time): " << speedup << "x" << std::endl;
+	const char* output_image_path_parallel_shared = "output_image_parallel_shared.jpg";
+
+	cudaMemset(d_output_image, 0, image_size); // Reset output image memory to zero
+
+	cudaEventRecord(start);
+
+	convolution_kernel_shared << <numBlocks, threadsPerBlock >> > (d_input_image, d_output_image, width, height, output_channels, d_filter, FILTER_DIM);
+
+	cudaEventRecord(stop);
+	cudaEventSynchronize(stop);
+
+	float elapsed_time_gpu_shared = 0.0f;
+	cudaEventElapsedTime(&elapsed_time_gpu_shared, start, stop);
+	std::cout << "3. Parallel Convolution (GPU - Shared Memory) completed in " << elapsed_time_gpu_shared << " ms" << std::endl;
+
+	cudaMemcpy(output_image, d_output_image, image_size, cudaMemcpyDeviceToHost);
+
+	// Save the output image
+	stbi_write_jpg(output_image_path_parallel_shared, width, height, output_channels, output_image, quality);
+
+	// speedup
+	std::cout << "\nSpeedup (CPU time / GPU time): " << elapsed_time_cpu.count() / elapsed_time_gpu << "x" << std::endl;
+	std::cout << "Speedup (GPU time / GPU time shared): " << elapsed_time_gpu / elapsed_time_gpu_shared << "x" << std::endl;
+	std::cout << "Speedup (CPU time / GPU time shared): " << elapsed_time_cpu.count() / elapsed_time_gpu_shared << "x" << std::endl;
+
+	// output image paths
+	std::cout << "\nAll the output images have been saved in different path: " << std::endl;
+	std::cout << "1. Serial Convolution (CPU): " << output_image_path_serial << std::endl;
+	std::cout << "2. Parallel Convolution (GPU): " << output_image_path_parallel << std::endl;
+	std::cout << "3. Parallel Convolution (GPU - Shared Memory): " << output_image_path_parallel_shared << std::endl;
 
 	// Free allocated memory
 	delete[] output_image;
